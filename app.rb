a3b3a8a283f6825b18654f3463b856cd59b9fa77
Sofias15 <anasofiasanch@gmail.com>
2018-06-27 14:54:31 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,18 +1,44 @@
-require_relative 'config/environment'
+require_relative #'config/environment'
+
+#class App < Sinatra::Base
+
+  # This is a sample static route.
+#  get '/hello' do
+    #"Hello World!"
+#  end
+
+  # This is a sample dynamic route.
+  #  @user_name = params[:name]
+   # "Hello #{@user_name}!"
+  #end
+
+  # Code your final two routes here:
+ # get '/medicines/:'do 
+    #@medicine = all_the_medicines.select do |medicine|
+    #  medicine.id== params[:id]
+   # end.first 
+   # erb :'/medicine/show.html'
+   # end
+    #####################################################
+
 
 class App < Sinatra::Base
 
   # This is a sample static route.
-  get '/hello' do
-    "Hello World!"
+  get '/goodbye' do
+    "goodbye!"
   end
 
   # This is a sample dynamic route.
-  get "/hello/:name" do
+  get "/goodbye/:name" do
     @user_name = params[:name]
-    "Hello #{@user_name}!"
+    "goodbye #{@user_name}!"
   end
 
   # Code your final two routes here:
-
-end
+  get '/medicines/:'do 
+    @medicine = all_the_medicines.select do |medicine|
+      medicine.id== params[:id]
+    end.first 
+    erb :'/medicine/show.html'
+    end
